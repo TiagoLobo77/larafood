7f4cd144008491d501e771b67f5cf5e9a72820e5
TiagoLobo77 <tiago_batista77@outlook.com>
2020-11-10 18:33:42 -0300

    Larafood.modulo10.aula.04

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
 Route::prefix('admin')
-    ->namespace('Admin')
-    ->middleware('auth')
-    ->group(function() { 
+        ->namespace('Admin')
+        ->middleware('auth')
+        ->group(function() {
+
+    /**
+     * Routes Users
+     */
+    Route::any('users/search', 'UserController@search')->name('users.search');
+    Route::resource('users', 'UserController');
 
     /**
      * Plan x Profile
@@ -13,44 +19,43 @@ Route::prefix('admin')
     Route::any('plans/{id}/profiles/create', 'ACL\PlanProfileController@profilesAvailable')->name('plans.profiles.available');
     Route::get('plans/{id}/profiles', 'ACL\PlanProfileController@profiles')->name('plans.profiles');
     Route::get('profiles/{id}/plans', 'ACL\PlanProfileController@plans')->name('profiles.plans');
-        
-     /**
-    * Permissions x Profile
-    */
-    Route::get('profiles/{id}/permission/{idPermission}/detach', 'ACL\PermissionProfileController@detachPermissionProfile')->name('profiles.permission.detach'); 
-    Route::post('profiles/{id}/permissions', 'ACL\PermissionProfileController@attachPermissionsProfile')->name('profiles.permissions.atach'); 
-    Route::any('profiles/{id}/permissions/create', 'ACL\PermissionProfileController@permissionsAvailable')->name('profiles.permissions.available'); 
-    Route::get('profiles/{id}/permissions', 'ACL\PermissionProfileController@permissions')->name('profiles.permissions'); 
-    Route::get('permissions/{id}/profile', 'ACL\PermissionProfileController@profiles')->name('permissions.profiles'); 
-        
-     /**
-    * Routes Permissions 
-    */
-    Route::any('permissions/search', 'ACL\PermissionController@search')->name('permissions.search'); 
-    Route::resource('permissions', 'ACL\PermissionController');     
 
     /**
-    * Routes Profiles 
-    */
-    Route::any('profiles/search', 'ACL\ProfileController@search')->name('profiles.search'); 
-    Route::resource('profiles', 'ACL\ProfileController');  
+     * Permission x Profile
+     */
+    Route::get('profiles/{id}/permission/{idPermission}/detach', 'ACL\PermissionProfileController@detachPermissionProfile')->name('profiles.permission.detach');
+    Route::post('profiles/{id}/permissions', 'ACL\PermissionProfileController@attachPermissionsProfile')->name('profiles.permissions.attach');
+    Route::any('profiles/{id}/permissions/create', 'ACL\PermissionProfileController@permissionsAvailable')->name('profiles.permissions.available');
+    Route::get('profiles/{id}/permissions', 'ACL\PermissionProfileController@permissions')->name('profiles.permissions');
+    Route::get('permissions/{id}/profile', 'ACL\PermissionProfileController@profiles')->name('permissions.profiles');
 
     /**
-    * 
-    */
+     * Routes Permissions
+     */
+    Route::any('permissions/search', 'ACL\PermissionController@search')->name('permissions.search');
+    Route::resource('permissions', 'ACL\PermissionController');
+
+    /**
+     * Routes Profiles
+     */
+    Route::any('profiles/search', 'ACL\ProfileController@search')->name('profiles.search');
+    Route::resource('profiles', 'ACL\ProfileController');
+
+    /**
+     * Routes Details Plans
+     */
     Route::delete('plans/{url}/details/{idDetail}', 'DetailPlanController@destroy')->name('details.plan.destroy');
     Route::get('plans/{url}/details/create', 'DetailPlanController@create')->name('details.plan.create');
     Route::get('plans/{url}/details/{idDetail}', 'DetailPlanController@show')->name('details.plan.show');
     Route::put('plans/{url}/details/{idDetail}', 'DetailPlanController@update')->name('details.plan.update');
     Route::get('plans/{url}/details/{idDetail}/edit', 'DetailPlanController@edit')->name('details.plan.edit');
     Route::post('plans/{url}/details', 'DetailPlanController@store')->name('details.plan.store');
-    Route::get('plans/{url}/details', 'DetailPlanController@index')->name('details.plan.index');    
+    Route::get('plans/{url}/details', 'DetailPlanController@index')->name('details.plan.index');
 
 
     /**
      * Routes Plans
      */
-
     Route::get('plans/create', 'PlanController@create')->name('plans.create');
     Route::put('plans/{url}', 'PlanController@update')->name('plans.update');
     Route::get('plans/{url}/edit', 'PlanController@edit')->name('plans.edit');
@@ -61,7 +66,7 @@ Route::prefix('admin')
     Route::get('plans', 'PlanController@index')->name('plans.index');
 
     /**
-     * Home Dashbord
+     * Home Dashboard
      */
     Route::get('/', 'PlanController@index')->name('admin.index');
 });
@@ -77,5 +82,3 @@ Route::get('/', 'Site\SiteController@index')->name('site.home');
  * Auth Routes
  */
 Auth::routes();
-
-
