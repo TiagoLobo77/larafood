05a011037eab8025880f2eb57d59da0195e4e2aa
TiagoLobo77 <tiago_batista77@outlook.com>
2020-12-29 03:33:26 -0300

    Larafood.modulo20.aula.06

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,14 @@
 <?php
 
+Route::post('/sanctum/token', 'Api\Auth\AuthClientController@auth');
+
+Route::group([
+    'middleware' => ['auth:sanctum']
+], function () {
+    Route::get('/auth/me', 'Api\Auth\AuthClientController@me');
+    Route::post('/auth/logout', 'Api\Auth\AuthClientController@logout');
+});
+
 Route::group([
     'prefix' => 'v1',
     'namespace' => 'Api'
@@ -15,21 +24,6 @@ Route::group([
 
     Route::get('/products/{flag}', 'ProductApiController@show');
     Route::get('/products', 'ProductApiController@productsByTenant');
-});
 
-Route::group([
-    'prefix' => 'v2',
-    'namespace' => 'Api'
-], function () {
-    Route::get('/tenants/{uuid}', 'TenantApiController@show');
-    Route::get('/tenants', 'TenantApiController@index');
-
-    Route::get('/categories/{url}', 'CategoryApiController@show');
-    Route::get('/categories', 'CategoryApiController@categoriesByTenant');
-
-    Route::get('/tables/{identify}', 'TableApiController@show');
-    Route::get('/tables', 'TableApiController@tablesByTenant');
-
-    Route::get('/products/{flag}', 'ProductApiController@show');
-    Route::get('/products', 'ProductApiController@productsByTenant');
+    Route::post('/client', 'Auth\RegisterController@store');
 });
